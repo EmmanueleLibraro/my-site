487952860c8ce38a29bf8c68e6e190af981cfb8d
EmmanueleLibraro <emmanuelelibraro95@gmail.com>
2021-05-05 17:23:48 +0200

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,47 @@
 <template>
     <header>
-        
+        <div class="header">
+            <a href="#">
+                <img src="@/assets/img/dc-logo.png" alt="Logo DC">
+            </a>
+
+            <div class="lista-header">
+                <ul>
+                    <li>
+                        <a href="#">Characters</a>
+                    </li>
+                    <li>
+                        <a href="#">Comic</a>
+                    </li>
+                    <li>
+                        <a href="#">Movies</a>
+                    </li>
+                    <li>
+                        <a href="#">Tv</a>
+                    </li>
+                    <li>
+                        <a href="#">Games</a>
+                    </li>
+                    <li>
+                        <a href="#">Collectibles</a>
+                    </li>
+                    <li>
+                        <a href="#">Videos</a>
+                    </li>
+                    <li>
+                        <a href="#">Fans</a>
+                    </li>
+                    <li>
+                        <a href="#">News</a>
+                    </li>
+                    <li>
+                        <a href="#">Shop</a>
+                    </li>
+
+                </ul>
+            </div>
+
+        </div>
     </header>
 </template>
 
@@ -11,5 +52,27 @@ export default {
 </script>
 
 <style>
+.header{
+    display: flex;
+    align-items: center;
+    width: 100%;
+    padding: 1rem;
+    font-family: sans-serif;
+}
+
+.lista-header ul{
+    display: flex;
+    justify-content: end;
+    list-style: none;
+    width: 100%;
+}
+
+.lista-header ul a{
+    text-decoration: none;
+    color: #000;
+    font-weight: bold;
+    margin-left: 1rem;
+}
+
 
 </style>
